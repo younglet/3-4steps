633c3315b9c8183ce77dd9433877279b5a32333e
younglet <younglet@qq.com>
2024-08-21 15:00:31 +0800

    piddemos

---
+++ 3-4-9/3_drone_simulator_pd.py
@@ -0,0 +1,51 @@
+from drone_simulator import Drone
+
+drone = Drone(target_height=100) # 初始化无人机，目标高度为100
+
+
+# 固定的升力让无人机停在目标高度，需要使用PID控制器来控制无人机的飞行
+
+def init():
+    global drone, dt, height_data, time_data
+    height_data = [0]
+    time_data = [0]
+    return line, 
+
+def  update(frame):
+    global drone, dt, height_data, time_data, last_error
+    error = drone.target_height - drone.height
+    derivative = (error - last_error) / dt
+    thrust = 0.1 * error  + 0.1 *  derivative  
+    drone.step(thrust, dt)
+    last_error = error
+
+    height_data.append(drone.height)
+    time_data.append(time_data[-1] + dt)
+    line.set_data(time_data, height_data)
+    return line,
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+
+
+# 创建图形
+fig, ax = plt.subplots()
+ax.set_xlim(0, 200)
+ax.set_ylim(0, 120)
+line, = ax.plot([], [], lw=2)
+dt = 1
+last_error = 0
+
+
+# 初始化数据
+height_data = [0]
+time_data = [0]
+
+# 创建动画
+ani = FuncAnimation(fig, update, frames=200, init_func=init, interval=1, repeat=False)
+
+plt.show()
---
+++ 3-4-9/drone_simulator.py
@@ -0,0 +1,93 @@
+class Drone:
+    """
+    模拟无人机类
+    """
+    def __init__(self,  target_height):
+        """
+        初始化无人机
+        :param initial_height: 初始高度
+        :param target_height: 目标高度
+        """
+        self.target_height = target_height
+        self.height = 0
+        self.velocity = 0
+        self.acceleration = 0
+        self.weight = 100
+        self.power = 200
+    
+    def step(self, thrust, dt):
+        """
+        计算无人机下一时刻的状态
+        :param thrust: 推力
+        :param dt: 时间间隔
+        """
+
+        if self.height >= 0:
+            if thrust > 1:
+                thrust = 1
+            if thrust < -1:
+                thrust = -1
+            self.acceleration = (thrust * self.power- self.weight)/ self.weight 
+            self.velocity += self.acceleration * dt
+            if abs(self.velocity) >4:
+                self.velocity = 4 * self.velocity/abs(self.velocity)
+            self.velocity *= 0.9
+        else:
+            self.acceleration = 0
+            self.velocity = 0
+        self.height += self.velocity * dt
+        self.log()
+
+    def log(self):
+        """
+        打印无人机当前状态
+        """
+        print(f"当前高度: {self.height}, 当前速度: {self.velocity}, 当前加速度: {self.acceleration}")
+class Drone:
+    """
+    模拟无人机类
+    """
+    def __init__(self,  target_height):
+        """
+        初始化无人机
+        :param initial_height: 初始高度
+        :param target_height: 目标高度
+        """
+        self.target_height = target_height
+        self.height = 0
+        self.velocity = 0
+        self.acceleration = 0
+        self.weight = 100
+        self.power = 200
+    
+    def step(self, thrust, dt):
+        """
+        计算无人机下一时刻的状态
+        :param thrust: 推力
+        :param dt: 时间间隔
+        """
+        if self.height >= 0:
+            self.acceleration = (thrust * self.power- self.weight)/ self.weight 
+            self.velocity += self.acceleration * dt
+            if abs(self.velocity) >4:
+                self.velocity = 4 * self.velocity/abs(self.velocity)
+            self.velocity *= 0.9
+        else:
+            self.acceleration = 0
+            self.velocity = 0
+        self.height += self.velocity * dt
+        self.log()
+    
+    def log(self):
+        """
+        打印无人机当前状态
+        """
+        print(f"当前高度: {self.height:.2f}, 当前速度: {self.velocity: .4f}, 当前加速度: {self.acceleration: .4f}")
+if __name__ == '__main__':
+
+    drone = Drone(target_height=100) # 初始化无人机，目标高度为100
+    for i in range(50):
+        drone.step(thrust=100, dt=1)# 1秒内提供固定的10N升力
+        drone.log()
+
+    # 固定的升力无法让无人机停在目标高度，需要使用PID控制来控制无人机的飞行
